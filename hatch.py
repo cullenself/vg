ae6995b2e7ae7b71d4d1d5e36f5e00abde72bae3
Cullen Self <cullen.self@utexas.edu>
2018-05-31 10:56:56 -0500

    Change into functions

--- hatch.py
+++ hatch.py
@@ -8,61 +8,61 @@ import svgwrite
 ## TODO: use polylines to make contours to make a mask to find the average value to color the triangles
 # gui to pick number of points and edge algorithm
 # color and fill options
-maxDensity = 2500000
-Q = lambda theta: np.matrix([[math.cos(theta), -math.sin(theta)],[math.sin(theta),math.cos(theta)]])
+def hatch(fn,maxDensity=2500000,randAngle=True,angle=45, high_edge=500, low_edge=100, count_features=500):
+    Q = lambda theta: np.matrix([[math.cos(theta), -math.sin(theta)],[math.sin(theta),math.cos(theta)]])
+    Q = Q if randAngle else Q(angle)
 
-print('Processing')
-img = cv2.imread('input2.jpg')
-height, width, _ = img.shape
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-edges = cv2.Canny(gray,100,500)
-out = np.ones(gray.shape, np.uint8)*255
+    print('Processing')
+    img = cv2.imread(fn)
+    height, width, _ = img.shape
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray,low_edge,high_edge)
+    out = np.ones(gray.shape, np.uint8)*255
 
-# Instantiate SVG Output
-dwg = svgwrite.Drawing('hatch.svg', size=(width,height))
+    # Instantiate SVG Output
+    dwg = svgwrite.Drawing('hatch.svg', size=(width,height))
 
-corners = cv2.goodFeaturesToTrack(edges,500,0.01,10)
-corners = np.int0(corners)
-subdiv = cv2.Subdiv2D((0,0,width,height))
+    corners = cv2.goodFeaturesToTrack(edges,count_features,0.01,10)
+    corners = np.int0(corners)
+    subdiv = cv2.Subdiv2D((0,0,width,height))
 
-for i in corners:
-    x, y = i.ravel()
-    subdiv.insert((x,y))
+    for i in corners:
+        x, y = i.ravel()
+        subdiv.insert((x,y))
 
-triList = subdiv.getTriangleList()
-i = 0
-print('Drawing')
-for t in triList:
-    i = i+1
-    pts = np.array([(t[0], t[1]), (t[2], t[3]), (t[4], t[5])]).reshape((-1,1,2)).astype(np.int32)
-    temp = np.zeros(gray.shape, np.uint8)
-    mask = np.zeros(gray.shape, np.uint8)
-    cv2.drawContours(mask, [pts], 0, 255, -1)
-    x,y,w,h = cv2.boundingRect(pts)
-    maxDim = max(w,h)
-    numLines = int(cv2.mean(gray, mask = mask)[0]/(255*w*h) * maxDensity)
-    if (numLines > 1):
-        theta = randint(0,90)
-        c = np.array([x+w/2,y+h/2])
-        verts = np.linspace(y-2*maxDim,y+2*maxDim,6*numLines)
-        lines = [[c+np.dot(Q(theta),np.array([x-maxDim,int(y)])-c), c+np.dot(Q(theta),np.array([x+2*maxDim,int(y)])-c)] for y in verts] 
-        # todo rotate
-        cv2.polylines(out, [pts], True, 0)
-        pts = [(int(p[0,0]),int(p[0,1])) for p in pts]
-        dwg.add(dwg.polyline(points=pts, stroke='black', fill='none'))
-        clip = dwg.defs.add(dwg.clipPath(id='cp_'+str(i)))
-        clip.add(dwg.polyline(points=pts))
-        for line in lines:
-            p1 = (int(line[0][0,0]), int(line[0][0,1]))
-            p2 = (int(line[1][0,0]), int(line[1][0,1]))
-            cv2.line(temp, p1, p2, 1)
-            dwg.add(dwg.line(start=p1,end=p2,stroke='black',clip_path='url(#cp_'+str(i)+')'))
-        out = out + cv2.bitwise_and(mask,temp)
+    triList = subdiv.getTriangleList()
+    i = 0
+    print('Drawing')
+    for t in triList:
+        i = i+1
+        pts = np.array([(t[0], t[1]), (t[2], t[3]), (t[4], t[5])]).reshape((-1,1,2)).astype(np.int32)
+        temp = np.zeros(gray.shape, np.uint8)
+        mask = np.zeros(gray.shape, np.uint8)
+        cv2.drawContours(mask, [pts], 0, 255, -1)
+        x,y,w,h = cv2.boundingRect(pts)
+        maxDim = max(w,h)
+        numLines = int(cv2.mean(gray, mask = mask)[0]/(255*w*h) * maxDensity)
+        if (numLines > 1):
+            R = Q(randint(0,90)) if randAngle else Q
+            c = np.array([x+w/2,y+h/2])
+            verts = np.linspace(y-2*maxDim,y+2*maxDim,6*numLines)
+            lines = [[c+np.dot(R,np.array([x-maxDim,int(y)])-c), c+np.dot(R,np.array([x+2*maxDim,int(y)])-c)] for y in verts] 
+            cv2.polylines(out, [pts], True, 0)
+            pts = [(int(p[0,0]),int(p[0,1])) for p in pts]
+            dwg.add(dwg.polyline(points=pts, stroke='black', fill='none'))
+            clip = dwg.defs.add(dwg.clipPath(id='cp_'+str(i)))
+            clip.add(dwg.polyline(points=pts))
+            for line in lines:
+                p1 = (int(line[0][0,0]), int(line[0][0,1]))
+                p2 = (int(line[1][0,0]), int(line[1][0,1]))
+                cv2.line(temp, p1, p2, 1)
+                dwg.add(dwg.line(start=p1,end=p2,stroke='black',clip_path='url(#cp_'+str(i)+')'))
+            out = out + cv2.bitwise_and(mask,temp)
 
-#cv2.imshow('rect',gray)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
+    #cv2.imshow('rect',gray)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
 
-print('Saving')
-cv2.imwrite('hatch.jpg',out)
-dwg.save()
+    print('Saving')
+    cv2.imwrite('hatch.jpg',out)
+    dwg.save()
--- waves.py
+++ waves.py
@@ -5,35 +5,28 @@ import math
 import svgwrite
 
 ## TODO: convert to gui with sliders for params and variable input
-scale = 20 
-sin_scale = 20
-pix_per_cycle = 10 
+def waves(fn,scale=20,sin_scale=20,pix_per_cycle=10):
+    img = cv2.imread(fn)
+    height, width, _ = img.shape
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-img = cv2.imread('face.jpg')
-height, width, _ = img.shape
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    mini = cv2.resize(gray, None, fx = 1, fy = 1/float(scale), interpolation = cv2.INTER_AREA)
+    mini_blur = cv2.GaussianBlur(mini, (5,5), 0)
+    inv_mini_blur = cv2.bitwise_not(mini_blur)
+    out = np.ones(gray.shape, np.uint8)*255
 
-mini = cv2.resize(gray, None, fx = 1, fy = 1/float(scale), interpolation = cv2.INTER_AREA)
-mini_blur = cv2.GaussianBlur(mini, (5,5), 0)
-inv_mini_blur = cv2.bitwise_not(mini_blur)
-out = np.ones(gray.shape, np.uint8)*255
+    # Instantiat SVG Output
+    dwg = svgwrite.Drawing('waves.svg', size=(width,height))
 
-# Instantiat SVG Output
-dwg = svgwrite.Drawing('waves.svg', size=(width,height))
+    xs = np.arange(0,mini.shape[1])
+    ys = (inv_mini_blur/float(255))*sin_scale*np.sin(xs*2*math.pi/pix_per_cycle) + (np.matrix(range(0,mini.shape[0])).T + 0.5)*height/mini.shape[0]
 
-xs = np.arange(0,mini.shape[1])
-ys = (inv_mini_blur/float(255))*sin_scale*np.sin(xs*2*math.pi/pix_per_cycle) + (np.matrix(range(0,mini.shape[0])).T + 0.5)*height/mini.shape[0]
+    for row in ys:
+        pts = zip(xs, np.asarray(row).squeeze())
+        pts = np.array(pts).reshape((-1,1,2)).astype(np.int32)
+        cv2.polylines(out, [pts], False, 0)
+        pts = [(int(p[0,0]),int(p[0,1])) for p in pts]
+        dwg.add(dwg.polyline(points=pts, stroke='black', fill='none'))
 
-for row in ys:
-    pts = zip(xs, np.asarray(row).squeeze())
-    pts = np.array(pts).reshape((-1,1,2)).astype(np.int32)
-    cv2.polylines(out, [pts], False, 0)
-    pts = [(int(p[0,0]),int(p[0,1])) for p in pts]
-    dwg.add(dwg.polyline(points=pts, stroke='black', fill='none'))
-
-#cv2.imshow('rect',mini_blur)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
-
-cv2.imwrite('waves.jpg',out)
-dwg.save()
+    cv2.imwrite('waves.jpg',out)
+    dwg.save()
